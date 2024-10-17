7658766ce00dea8686847713357ea215c9e8bd96
muriloSanches1001 <143918265+muriloSanches1001@users.noreply.github.com>
2024-10-17 10:48:40 -0300

    User Controller and DB Config Alterations

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,39 @@
-﻿namespace api_tarefa.Controllers
+﻿using api_tarefa.DBContext;
+using api_tarefa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_tarefa.Controllers
 {
-    public class UserController
+    [Route("api/[controller]")] //localhost/api/user/name
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly AppDBContext _dbContext;
+
+        public UserController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("find-all")]
+        public async Task<ActionResult<IEnumerable<User>>> FindAll()
+        {
+            return await _dbContext.users.ToListAsync();
+        }
+
+        [HttpGet("find-by-id/{id}")]
+        public async Task<ActionResult<User>> FindById(int id)
+        {
+            return await _dbContext.users.FindAsync(id);
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<User>> Create(User user)
+        {
+            _dbContext.users.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return Ok(user);
+        }
     }
 }
